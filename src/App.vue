cc2aaa8554af58eea343f669e8a87bab38590feb
Samuele-Chioda <samuelechioda@gmail.com>
2024-06-12 17:08:17 +0200

    scaffolding base-installazione bootstrap-installazione sass

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  @use'./styles/general.scss';
+  @use'./node_modules/bootstrap/scss/bootstrap.scss';
+  @use'./styles/partials/mixins' as*;
+  @use'./styles/partials/variables' as*;
+
+</style>
