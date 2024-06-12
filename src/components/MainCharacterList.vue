967cd6c5040f519d50eeffd3e85d3a5ba85cbbf9
Samuele-Chioda <samuelechioda@gmail.com>
2024-06-12 22:11:44 +0200

    aggiunta style card

--- src/components/MainCharacterList.vue
+++ src/components/MainCharacterList.vue
@@ -24,17 +24,17 @@ export default {
         <div class="row">
             <div class="col-12">
                 <h2>
-                    Main Chars
+                    Cards
                 </h2>
             </div>
             <div class="col-12">
                 <div class="row">
                     <article v-for="character in characters" :key="character.id" class="col-3">
-                        <div class="card" style="width: 100%;">
+                        <div id="card" class="card" style="width: 100%;">
                             <img class="card-img-top" :src="character.card_images[0].image_url" alt="Card image cap">
                             <div class="card-body">
-                                <h5 class="card-title">{{ character.name }}</h5>
-                                <p class="card-text">{{ character.desc }}</p>
+                                <h5 id="color-white" class="card-title fw-bold">{{ character.name }}</h5>
+                                <p class="card-text fs-6">{{ character.type }}</p>
                             </div>
                         </div>
                     </article>
@@ -46,7 +46,28 @@ export default {
 
 
 <style lang="scss">
+
+h2{
+    text-align: center;
+    padding: 1rem;
+}
+
 .container {
     background-color: white;
 }
+
+#card{
+    background-color: #d48f38;
+    text-align: center;
+    margin-bottom: .5rem;
+}
+
+h5{
+    text-transform: uppercase;
+}
+
+#color-white{
+    color: white;
+}
+
 </style>
